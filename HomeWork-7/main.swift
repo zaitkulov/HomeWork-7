12a9700df591ecac0fb5cfa4d827bdb72d887eee
zaitkulov <zaitkulov@gmail.com>
2023-12-15 16:54:48 +0500

    HomeWork-7-1.Создание класса Car

--- HomeWork-7/main.swift
+++ HomeWork-7/main.swift
@@ -7,5 +7,26 @@
 
 import Foundation
 
-print("Hello, World!")
+//1.Задание: Создание класса Car
+
+class Car {
+    var brand: String
+    var model: String
+    var year: Int
+    
+    init(brand: String, model: String, year: Int) {
+        self.brand = brand
+        self.model = model
+        self.year = year
+    }
+    func performAction() {
+        print ("Автомобиль выполняет действие")
+    }
+}
+
+let myCar = Car(brand: "Tesla", model: "Model X", year: 2022)
+print ("Моя машина - \(myCar.brand) \(myCar.model) \(myCar.year) года")
+
+myCar.performAction()
+
 
