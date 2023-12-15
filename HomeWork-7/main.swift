6592d7566f45b7d2e6ce7054ad486a7fd694984d
zaitkulov <zaitkulov@gmail.com>
2023-12-15 17:10:28 +0500

    HomeWork-7-2.Создание подклассов

--- HomeWork-7/main.swift
+++ HomeWork-7/main.swift
@@ -29,4 +29,49 @@ print ("Моя машина - \(myCar.brand) \(myCar.model) \(myCar.year) год
 
 myCar.performAction()
 
+//2.Задание: Создание подклассов TrunkCar и SportCar
+
+class Car {
+    var brand: String
+    var year: Int
+    
+    init(brand: String, year: Int) {
+        self.brand = brand
+        self.year = year
+    }
+    func startEngine() {
+        print ("Двигатель запущен")
+    }
+}
+class TrunkCar: Car {
+    var cargoCapacity: Int
+
+    init(brand: String, year: Int, cargoCapacity: Int) {
+        self.cargoCapacity = cargoCapacity
+        super.init(brand: brand, year: year)
+    }
+    func loadCargo() {
+        print("Грузовик загружен")
+    }
+}
+class SportCar: Car {
+    var maxSpeed: Int
+    
+    init(brand: String, year: Int, maxSpeed: Int) {
+        self.maxSpeed = maxSpeed
+        super.init(brand: brand, year: year)
+    }
+    func accelerate() {
+        print ("Разгон до максимальной скорости")
+    }
+}
+let trunkCar = TrunkCar(brand: "Volvo", year: 2018, cargoCapacity: 40)
+trunkCar.startEngine()
+trunkCar.loadCargo()
+print("Грузоподъемность: \(trunkCar.cargoCapacity) кг")
+
+let sportCar = SportCar(brand: "Спорткар", year: 2022, maxSpeed: 3000)
+sportCar.startEngine()
+sportCar.accelerate()
+print("Максимальная скорость: \(sportCar.maxSpeed) км/ч")
 
